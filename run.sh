dfcc1d0efcd3223ed87ec131d6f213a01499776e
laucha12 <lautihernando@gmail.com>
2022-10-01 12:04:22 -0300

    GDB setUp

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [[ "$1" = "gdb" ]];then
-    qemu-system-x86_64 -s -S -hda Image/x64BareBonesImage.qcow2 -m 512 -d int
+    qemu-system-x86_64 -s -S -hda Image/x64BareBonesImage.qcow2 -m 512 -d int -monitor stdio
 else
     qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512
 fi
