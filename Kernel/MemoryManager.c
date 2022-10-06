d4faa66e6441484dd404fd16772242c03eea601a
Marco444 <elscili444.code@gmail.com>
2022-10-06 16:50:52 -0300

    added second memory manager functionality

--- Kernel/MemoryManager.c
+++ Kernel/MemoryManager.c
@@ -3,37 +3,58 @@
 #include "./include/buddy.h"
 #include <stdlib.h>
 
+#define BUDDY BUDDY  
+
 typedef struct MemoryManagerCDT {
-	Buddy buddyManager;	
+	Buddy manager;	
 	void * startingMemory;
 } MemoryManagerCDT;
 
+
 void createMemoryManager(void *const restrict memoryForMemoryManager, void *const restrict managedMemory) {
-	
+
 	//definimos al manager de manera hardcodeada porque no puede alocar memoria para si mismo
 	memoryManager = (MemoryManagerADT) memoryForMemoryManager;	
 
 	//inicializamos al buddy del memory manager, junto con la posicion desde donde almacena el buddy
 	//junto con la memoria que administra.
-	memoryManager->buddyManager = buddy_new(MAX_MEMORY, managedMemory);
+	#ifdef BUDDY 
+  	memoryManager->manager = buddy_new(MAX_MEMORY, managedMemory);
+	#else 
+
+	#endif
+
 	memoryManager->startingMemory = managedMemory;
 }
 
 void *allocMemory(const int memoryToAllocate) {
-	
-	if(memoryToAllocate == 0) return NULL;
- 
- 	//le pido al buddy que aloquea una memoria
-  int offset = buddy_alloc(memoryManager->buddyManager, memoryToAllocate);
 
-	//devuelvo desde el comienzo de memoria del buddy mas el offset. TODO, puede
-	//ser que no este haciendo bien bien el offset, verificar con GDB.
-	return (void *) (memoryManager->startingMemory + offset);
+	void * addr = NULL;
+
+	if(memoryToAllocate == 0) return addr;
+
+	#ifdef BUDDY 
+ 		//le pido al buddy que aloquea una memoria
+  	int offset = buddy_alloc(memoryManager->manager, memoryToAllocate);
+
+		//devuelvo desde el comienzo de memoria del buddy mas el offset. TODO, puede
+		//ser que no este haciendo bien bien el offset, verificar con GDB.
+		addr = (void *) (memoryManager->startingMemory + offset);
+	#else 
+	
+	#endif
+
+	return addr;
 }
 
 void freeMemory (void * const address) {
 
 	if(address == NULL) return; 
-	
-	buddy_free(memoryManager->buddyManager, (int) (address - memoryManager->startingMemory));
+
+	#ifdef BUDDY
+		buddy_free(memoryManager->manager, (int) (address - memoryManager->startingMemory));
+	#else
+		
+	#endif
 }
+
