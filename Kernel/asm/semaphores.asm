aaa1040719f9f44d877af44944e2013d9f75837f
martinippo01 <martinippo@gmail.com>
2022-10-27 14:46:01 -0300

    Changes

---
+++ Kernel/asm/semaphores.asm
@@ -0,0 +1,17 @@
+GLOBAL try_lock
+GLOBAl unlock
+
+; This routine will check the value untill it is 0
+; rdi -> Pointer to the value we want to check
+try_lock:
+    mov rax, 0
+    mov al, 1
+    xchg al, [rdi]
+    cmp al, 0
+    jne try_lock
+    ret
+
+;   rdi -> Pointer to the value we want to set as 0
+unlock:
+    mov byte [rdi], 0
+    ret
