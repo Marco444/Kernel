33d7fb8041c5437c637d334bb5003f127abaa51b
martinippo01 <martinippo@gmail.com>
2022-10-30 11:16:22 -0300

    Changes to Compile

--- Kernel/asm/semaphores.asm
+++ Kernel/asm/semaphores.asm
@@ -3,12 +3,12 @@ GLOBAl unlock
 
 ; This routine will check the value untill it is 0
 ; rdi -> Pointer to the value we want to check
-try_lock:
+tryLock:
     mov rax, 0
     mov al, 1
     xchg al, [rdi]
     cmp al, 0
-    jne try_lock
+    jne tryLock
     ret
 
 ; tryLock:
