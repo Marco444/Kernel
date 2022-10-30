a4a1cbb02b5ba4d087e4e66b5c1a5a7c06197430
Marco444 <elscili444.code@gmail.com>
2022-10-30 15:33:26 -0300

    changes

--- Userland/SampleCodeModule/testManager.c
+++ Userland/SampleCodeModule/testManager.c
@@ -19,6 +19,9 @@ void testManagerRun(Window window, int argc,
     newLine(window);
   }
 
+  puts_("Running memory manager test ... \n", window);
+  testMM(window, -1, 10);
+  return;
   if (argc == 2 && strcmp_(argv[1], "memory") == 0) {
 
     puts_("Running memory manager test ... \n", window);
@@ -28,9 +31,9 @@ void testManagerRun(Window window, int argc,
     puts_("Running processes test ...", window);
     testProcesses(window, argc, argv);
   } else {
-    //testProcesses(window, argc, argv);
+    // testProcesses(window, argc, argv);
     puts_("Running Syncronisation test ... \n", window);
-    char arguments[2][2]={"3", "1"};
+    char arguments[2][2] = {"3", "1"};
     testSync(2, arguments);
   }
 }
