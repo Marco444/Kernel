dbb937effbc82068ed9ca3bec58ebad896f2bf98
martinippo01 <martinippo@gmail.com>
2022-10-30 11:09:40 -0300

    minor changes

--- Kernel/asm/semaphores.asm
+++ Kernel/asm/semaphores.asm
@@ -3,26 +3,25 @@ GLOBAl unlock
 
 ; This routine will check the value untill it is 0
 ; rdi -> Pointer to the value we want to check
-; try_lock:
-;     mov rax, 0
-;     mov al, 1
-;     xchg al, [rdi]
-;     cmp al, 0
-;     ; Check what will do -> Shall we block a process the call int 0x20 for timer tick?
-;     jne try_lock
-;     ret
-
-tryLock:
+try_lock:
     mov rax, 0
     mov al, 1
     xchg al, [rdi]
     cmp al, 0
-    jne case_not
-    mov rax, 1
+    jne try_lock
     ret
-    case_not:
-    mov rax, 0
-    ret
+
+; tryLock:
+;     mov rax, 0
+;     mov al, 1
+;     xchg al, [rdi]
+;     cmp al, 0
+;     jne case_not
+;     mov rax, 1
+;     ret
+;     case_not:
+;     mov rax, 0
+;     ret
 
 ;   rdi -> Pointer to the value we want to set as 0
 unlock:
