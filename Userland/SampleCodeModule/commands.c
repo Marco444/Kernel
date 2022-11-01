68249e974a15ff298a31579a2e26f1c6aaab4d40
laucha12 <lautihernando@gmail.com>
2022-11-01 18:06:12 -0300

    minor changes

--- Userland/SampleCodeModule/commands.c
+++ Userland/SampleCodeModule/commands.c
@@ -33,7 +33,7 @@ void kill(int argc, char argv[MAX_ARGUMENT_COUNT][MAX_ARGUMENT]) {
 void nice(int argc, char argv[MAX_ARGUMENT_COUNT][MAX_ARGUMENT]) {
   verifyArguments(argc, 3);
   sysNiceProcess(atoi_(argv[1]), atoi_(argv[2]));
-  exit(0);
+  exit_(0);
 }
 
 void block(int argc, char argv[MAX_ARGUMENT_COUNT][MAX_ARGUMENT]) {
